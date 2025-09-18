efce2d2d36535ca4e3ce4cf2520fa5e04c9dae1a
Parashar Das <porashorbabu@gmail.com>
2025-09-18 15:17:11 +0600

    host cors setup on preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   preview: {
+    port: 4000,
+    host: true,
+    allowedHosts: ['chat-xuk2.onrender.com']
+  }
 })
